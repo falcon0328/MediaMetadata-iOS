a666f358b3307da4a440b183a3bdef7f413aab12
aseo <aseo@yahoo-corp.jp>
2019-01-06 14:17:27 +0900

    Initial Commit

---
+++ MediaMetadata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaMetadata
+//
+//  Created by aseo on 2019/01/06.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
