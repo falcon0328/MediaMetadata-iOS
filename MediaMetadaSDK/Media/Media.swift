faedb29ac27cb718d8ec90e5e2383292afb28348
aseo <aseo@yahoo-corp.jp>
2019-01-07 19:25:59 +0900

    docs: メディア型のドキュメントコメントを追加 #1

--- MediaMetadaSDK/Media/Media.swift
+++ MediaMetadaSDK/Media/Media.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+/// メタデータを含む画像や動画などのメディアデータを表現するためのプロトコル
+public protocol Media {
+    /// メディアの種類
+    var type: MediaType { get }
+    /// カメラロール内でのローカル識別子
+    var localIdentifier: String { get }
+    /// メディアのバイトデータ
+    var rawData: Data { get }
+    /// メディアのメタデータ
+    var metadata: [MetadataKey: Metadata] { get set }
+}
