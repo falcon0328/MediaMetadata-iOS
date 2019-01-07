94d089ad2bf82eb223864da134b86664be1baef1
aseo <aseo@yahoo-corp.jp>
2019-01-07 19:33:51 +0900

    docs: MediaLibraryManagerのプロトコルを実装 #1

--- MediaMetadaSDK/Media/MediaLibraryManager.swift
+++ MediaMetadaSDK/Media/MediaLibraryManager.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+/// メディアを管理するためのプロトコル
+///
+/// - attention:
+/// カメラロールの写真や動画を本SDKではメディアと呼んでいる
+public protocol MediaLibraryManager {
+    /// メディアの読み込み
+    ///
+    /// - Parameter localIdentifier: カメラロールでのローカル識別子
+    /// - Returns: 該当するメディア
+    func load(localIdentifier: String) -> Media?
+    /// メディアの削除
+    ///
+    /// - Parameter localIdentifier: カメラロールでのローカル識別子
+    /// - Returns: 削除の成否
+    func delete(localIdentifier: String) -> Bool
+}
