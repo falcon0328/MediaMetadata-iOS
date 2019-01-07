5e571154c57a07b17a5ae2de7e18ec89dbe6c939
aseo <aseo@yahoo-corp.jp>
2019-01-08 02:10:30 +0900

    feat: MetadataKeyを利用するようにした #2

--- MediaMetadaSDK/Metadata/Metadata.swift
+++ MediaMetadaSDK/Metadata/Metadata.swift
@@ -11,18 +11,18 @@ import Foundation
 /// メタデータを表現するためのプロトコル
 public protocol Metadata {
     /// メタデータを格納した辞書
-    var rawValue: [String: Any] { get set }
+    var rawValue: [MetadataKey: Any] { get set }
     /// メタデータの追加
     ///
     /// - Parameters:
     ///   - metadataKey: メタデータのキー
     ///   - value: メタデータとして格納したい値
     /// - Returns: 追加の成否
-    func write(metadataKey: String, value: Any) -> Bool
+    func write(metadataKey: MetadataKey, value: Any) -> Bool
     /// 指定したキー値を持つメタデータの読み込み
     ///
     /// 指定したキー値を持つメタデータが存在しない場合、nilを返す
     /// - Parameter metadataKey: メタデータのキー
     /// - Returns: 指定したキー値を持つメタデータ
-    func read(metadataKey: String) -> Any?
+    func read(metadataKey: MetadataKey) -> Any?
 }
