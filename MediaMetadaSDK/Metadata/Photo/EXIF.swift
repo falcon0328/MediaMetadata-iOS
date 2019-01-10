4b1eda02d9785ede35d578aed30cdf99299bd813
aseo <aseo@yahoo-corp.jp>
2019-01-10 12:39:03 +0900

    feat: 各メタデータのファイルを追加 #2

---
+++ MediaMetadaSDK/Metadata/Photo/EXIF.swift
@@ -0,0 +1,9 @@
+//
+//  EXIF.swift
+//  MediaMetadaSDK
+//
+//  Created by aseo on 2019/01/10.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import Foundation
---
+++ MediaMetadaSDK/Metadata/Photo/IPTC.swift
@@ -0,0 +1,9 @@
+//
+//  IPTC.swift
+//  MediaMetadaSDK
+//
+//  Created by aseo on 2019/01/10.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import Foundation
