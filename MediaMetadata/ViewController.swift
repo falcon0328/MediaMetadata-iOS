6f5fca3c21d572efb8047c1d43eabfb3154aca1f
aseo <aseo@yahoo-corp.jp>
2019-01-13 21:07:36 +0900

    feat: テスト用ファイルとコードの追加 #3

--- MediaMetadata/ViewController.swift
+++ MediaMetadata/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Photos
+import MediaMetadaSDK
 
 class ViewController: UIViewController {
-
+    let exifUserCommentKey = "UserComment"
+    let array = ["BDF696F6-8C83-4C51-9051-804A5C0991CA/L0/001"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let kariExif = EXIF(rawValue: [exifUserCommentKey: "Hoge Fuga Piyo"])
+        
+        let medias = MediaLibrary.shared().load(localIdentifier: array)
+        for (index, media) in medias.enumerated() {
+            var media = medias.object(index: index)
+            media.setMetadata(key: .exif, value: kariExif)
+            media.save(completionHandler: { isSuccess, error in
+                print(isSuccess)
+            })
+        }
     }
 
 
