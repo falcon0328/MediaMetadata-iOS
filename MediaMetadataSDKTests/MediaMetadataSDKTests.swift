b618e61e0e4e2c147e5e1eea837430911edea8db
aseo <aseo@yahoo-corp.jp>
2019-01-14 22:09:01 +0900

    feat: MediaMetadaSDKのタイプミスに気づき、修正

--- MediaMetadataSDKTests/MediaMetadataSDKTests.swift
+++ MediaMetadataSDKTests/MediaMetadataSDKTests.swift
@@ -1,15 +1,15 @@
 //
-//  MediaMetadaSDKTests.swift
-//  MediaMetadaSDKTests
+//  MediaMetadataSDKTests.swift
+//  MediaMetadataSDKTests
 //
-//  Created by aseo on 2019/01/06.
+//  Created by aseo on 2019/01/14.
 //  Copyright © 2019年 Falcon Tech. All rights reserved.
 //
 
 import XCTest
-@testable import MediaMetadaSDK
+@testable import MediaMetadataSDK
 
-class MediaMetadaSDKTests: XCTestCase {
+class MediaMetadataSDKTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- MediaMetadataSDKTests/PhotoTests.swift
+++ MediaMetadataSDKTests/PhotoTests.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import XCTest
-@testable import MediaMetadaSDK
+@testable import MediaMetadataSDK
 
 class PhotoTests: XCTestCase {
     var photoUrl: URL {
