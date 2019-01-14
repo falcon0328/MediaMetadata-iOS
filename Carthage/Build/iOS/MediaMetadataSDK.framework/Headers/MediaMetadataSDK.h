0d963a4149eadff7767c965d07fe165c6345183e
aseo <aseo@yahoo-corp.jp>
2019-01-15 00:03:13 +0900

    feat: Carthageで配信するライブラリを更新

---
+++ Carthage/Build/iOS/MediaMetadataSDK.framework/Headers/MediaMetadataSDK.h
@@ -0,0 +1,19 @@
+//
+//  MediaMetadataSDK.h
+//  MediaMetadataSDK
+//
+//  Created by aseo on 2019/01/14.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MediaMetadataSDK.
+FOUNDATION_EXPORT double MediaMetadataSDKVersionNumber;
+
+//! Project version string for MediaMetadataSDK.
+FOUNDATION_EXPORT const unsigned char MediaMetadataSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaMetadataSDK/PublicHeader.h>
+
+
