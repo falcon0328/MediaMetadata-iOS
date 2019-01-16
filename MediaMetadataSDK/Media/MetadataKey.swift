544099a844e10e1b29bfa23fa8e0300c4203fac4
aseo <aseo@yahoo-corp.jp>
2019-01-16 10:28:40 +0900

    feat: メタデータのキー値を追加 #11

--- MediaMetadataSDK/Media/MetadataKey.swift
+++ MediaMetadataSDK/Media/MetadataKey.swift
@@ -20,4 +20,20 @@ public enum MetadataKey: String {
     case jfif = "{JFIF}"
     /// TIFF
     case tiff = "{TIFF}"
+    /// カラーモデル
+    case colorModel = "ColorModel"
+    /// 向き
+    case orientation = "Orientation"
+    /// 画像の幅
+    case pixelWidth = "PixelWidth"
+    /// 画像の高さ
+    case pixelHeight = "PixelHeight"
+    /// 画像の深度
+    case depth = "Depth"
+    /// プロファイル名
+    case profileName = "ProfileName"
+    /// 解像度の幅
+    case dpiWidth = "DPIWidth"
+    /// プライマリーイメージ
+    case primaryImage = "PrimaryImage"
 }
