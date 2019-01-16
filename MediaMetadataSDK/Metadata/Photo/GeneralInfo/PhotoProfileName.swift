a915f1dd4b24c096ec05cd4021c1ae1234ecec35
aseo <aseo@yahoo-corp.jp>
2019-01-16 10:54:15 +0900

    feat: rawValueが文字列になる予定のデータを修正 #11

--- MediaMetadataSDK/Metadata/Photo/GeneralInfo/PhotoProfileName.swift
+++ MediaMetadataSDK/Metadata/Photo/GeneralInfo/PhotoProfileName.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+/// 写真のプロファイル名を表す構造体
+public struct PhotoProfileName: Metadata {
+    public var rawValue: Any {
+        get {
+            return _rawValue
+        }
+    }
+    private var _rawValue: String = ""
+    
+    public init(rawValue: Any) {
+        guard let rawValue = rawValue as? String else {
+            return
+        }
+        _rawValue = rawValue
+    }
+    
+    public func write(metadataKey: String, value: Any) -> Bool {
+        return false
+    }
+    public mutating func write(value: Any) -> Bool {
+        guard let value = value as? String else {
+            return false
+        }
+        _rawValue = value
+        return true
+    }
+    
+    public func read(metadataKey: String) -> Any? {
+        return nil
+    }
+    public func read() -> Any {
+        return rawValue
+    }
+}
