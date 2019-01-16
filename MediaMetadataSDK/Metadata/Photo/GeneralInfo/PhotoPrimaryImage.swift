7537a41f77a817f50e509fb69d60a603364e1b0e
aseo <aseo@yahoo-corp.jp>
2019-01-16 11:07:39 +0900

    feat: Depth, DPIWidth, PrimaryImageのメタデータを表す構造体を実装 #11

--- MediaMetadataSDK/Metadata/Photo/GeneralInfo/PhotoPrimaryImage.swift
+++ MediaMetadataSDK/Metadata/Photo/GeneralInfo/PhotoPrimaryImage.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+/// プライマリーイメージ
+public struct PhotoPrimaryImage: Metadata {
+    public var rawValue: Any {
+        get {
+            return _rawValue
+        }
+    }
+    private var _rawValue: Int
+    
+    public init?(rawValue: Any) {
+        guard let rawValue = rawValue as? Int else {
+            return nil
+        }
+        _rawValue = rawValue
+    }
+    
+    public func write(metadataKey: String, value: Any) -> Bool {
+        return false
+    }
+    public mutating func write(value: Any) -> Bool {
+        guard let value = value as? Int else {
+            return false
+        }
+        _rawValue = value
+        return true
+    }
+    
+    public func read(metadataKey: String) -> Any? {
+        return nil
+    }
+    public func read() -> Any {
+        return rawValue
+    }
+}
