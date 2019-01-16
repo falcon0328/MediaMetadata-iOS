a3a9d02a5153ce17d8b17c0dbb8c899027001c75
aseo <aseo@yahoo-corp.jp>
2019-01-16 10:40:52 +0900

    feat: ファイル新規作成 #11

---
+++ MediaMetadataSDK/Metadata/Photo/GeneralInfo/PhotoColorModel.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoColorModel.swift
+//  MediaMetadataSDK
+//
+//  Created by aseo on 2019/01/16.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import Foundation
---
+++ MediaMetadataSDK/Metadata/Photo/GeneralInfo/PhotoPixelHeight.swift
@@ -0,0 +1,10 @@
+//
+//  PhotoPixelHeight.swift
+//  MediaMetadataSDK
+//
+//  Created by aseo on 2019/01/16.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+import Foundation
+
